7a83404e6386619104b4179812df3a11f676e047
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-05-30 01:03:42 +0100

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  uploadPhoto()
-    .then((photo) => {
-      createUser().then((user) => {
-        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-      });
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+export default async function handleProfileSignup() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (err) {
+    console.log('Signup system offline');
+  }
 }
