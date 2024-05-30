968ad92110972e8830e621ae51f109665ab1c1b0
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-05-30 01:00:48 +0100

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  uploadPhoto()
+    .then((photo) => {
+      createUser().then((user) => {
+        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+      });
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
