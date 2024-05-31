6dbd1eeb5eda002ef929d8a7a482c7d8af59e4cd
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-05-31 02:09:50 +0100

    10 and 100

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,18 +1,16 @@
 /* eslint-disable no-underscore-dangle */
-class Car {
-  // Constructor with attributes: brand, motor, color
+export default class Car {
   constructor(brand, motor, color) {
-    // Storing attributes with underscore prefix
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  // Method to clone the Car object
+  static get [Symbol.species]() {
+    return this.prototype.constructor;
+  }
+
   cloneCar() {
-    // Creating a new object of the Car class with the same attributes
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor[Symbol.species]();
   }
 }
-
-export default Car;
--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,25 +1,13 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable linebreak-style */
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line import/extensions
 import Car from './10-car';
 
-// Implement the EVCar class that extends the Car class
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+  static get [Symbol.species]() {
+    return Car;
   }
 }
-
-// Usage
-const myCar = new EVCar('Tesla', 'Electric', 'Red', '300 miles');
-console.log(myCar.cloneCar());
-
-export default EVCar;
