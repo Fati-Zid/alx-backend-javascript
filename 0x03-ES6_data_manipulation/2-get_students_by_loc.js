a5f72b75315708740ae06429f4c94b2f94d1535b
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-06-08 02:11:25 +0100

    update

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-import getListStudents from './0-get_list_students';
-
 export default function getStudentsByLocation(students, city) {
   return students.filters((student) => student.location === city);
 }
