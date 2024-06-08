f363f54e0313f7df89b97dd631e115c79b302b3f
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-06-08 01:08:09 +0100

    setup project: files, REadme, task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-return-assign */
+export default function getListStudents() {
+  return [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 3, firstname: 'Serena', location: 'San Francisco' },
+  ];
+}
